59d2f53b6ba4f2ca71e92656c4e8564b0272b9d8
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2022-09-12 23:29:46 +0300

    first edit

--- src/index.js
+++ src/index.js
@@ -6,19 +6,9 @@ var cors = require('cors')
  
 const app = express()
 app.use(cors())
-const port = process.env.PORT || 3030
+const port = process.env.PORT || 3040
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
 
 app.use(express.json())
 app.use(userRouter)
@@ -28,17 +18,5 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const Task = require('./models/task')
-const User = require('./models/user')
 
-// const main = async () => {
-//     // const task = await Task.findById('5c2e505a3253e18a43e612e6')
-//     // await task.populate('owner').execPopulate()
-//     // console.log(task.owner)
 
-//     const user = await User.findById('5c2e4dcb5eac678a23725b5b')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// main()
--- src/routers/task.js
+++ src/routers/task.js
@@ -6,7 +6,7 @@ const router = new express.Router()
 router.post('/tasks', async (req, res) => {
     const task = new Task({
         ...req.body,
-        // owner: req.user._id
+       
     })
 
     try {
@@ -26,21 +26,7 @@ router.get('/tasks', async (req, res) => {
     }
 })
 
-router.get('/tasks/:id', auth, async (req, res) => {
-    const _id = req.params.id
 
-    try {
-        const task = await Task.findOne({ _id, owner: req.user._id })
-
-        if (!task) {
-            return res.status(404).send()
-        }
-
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
 
 router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
